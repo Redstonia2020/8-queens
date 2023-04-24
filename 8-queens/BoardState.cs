13e9f7ba80accbb9c70ddac43a6ef9b02e589315
Redstonia <redstonia1234@gmail.com>
2023-04-25 10:15:15 +1200

    Add project files.

---
+++ 8-queens/BoardState.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_queens
+{
+    internal class BoardState
+    {
+        public int[] State = new int[8];
+
+        public BoardState()
+        {
+            for (int i = 0; i < State.Length; i++)
+            {
+                State[i] = -1;
+            }
+        }
+
+        public BoardState(int[] state)
+        {
+            State = state;
+        }
+
+        public void Iterate()
+        {
+            if (GetCurrentRow() == -1)
+            {
+                Console.WriteLine("A solution:");
+                DisplayBoard();
+                Console.ReadLine();
+                return;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (isValidPlacement(i))
+                    PlaceQueen(i).Iterate();
+            }
+        }
+
+        public BoardState PlaceQueen(int column)
+        {
+            int[] newState = new int[State.Length];
+            Array.Copy(State, newState, State.Length);
+            newState[GetCurrentRow()] = column;
+            return new BoardState(newState);
+        }
+
+        private bool isValidPlacement(int column)
+        {
+            return isColumnClear(column) && areDiagonalsClear(column);
+        }
+
+        private bool isColumnClear(int column)
+        {
+            for (int i = 0; i < GetCurrentRow(); i++)
+            {
+                if (State[i] == column)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool areDiagonalsClear(int column)
+        {
+            for (int i = 0; i < GetCurrentRow(); i++)
+            {
+                int distanceAway = GetCurrentRow() - i;
+                if (State[i] - distanceAway == column || State[i] + distanceAway == column)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private int GetCurrentRow()
+        {
+            return Array.IndexOf(State, -1);
+        }
+
+        public void DisplayBoard()
+        {
+            foreach (int columnInRow in State)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (columnInRow == i)
+                        Console.Write(" *");
+                    else
+                        Console.Write(" -");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 8-queens/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_queens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(multi-row support coming to stores near you");
+            Console.Write("Give column (number) to place queen in first row: ");
+            int queenStartColumn = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            BoardState board = new BoardState().PlaceQueen(queenStartColumn);
+
+            Console.WriteLine("Starting board:");
+            board.DisplayBoard();
+            Console.ReadLine();
+
+            board.Iterate();
+
+            Console.ReadLine();
+        }
+    }
+}
